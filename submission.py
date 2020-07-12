4622b440aeb939bd0607e45349330dce857516e8
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-07-13 00:27:45 +1000

    incorrect and pyspark implementations. TODO: fix optimisation

--- submission.py
+++ submission.py
@@ -35,28 +35,53 @@ def countCol(data_hash, query_hashes, offset, length):
     return count
 
 
+def c(data_hash, query_hashes, offset, length, alpha_m):
+    count = 0
+    k, v = data_hash
+
+    for i in range(length):
+        if abs(v[i] - query_hashes[i]) <= offset:
+            count += 1
+
+    if count >= alpha_m:
+        return k
+
 ########## Question 1 ##########
 # do not change the heading of the function
 def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
     offset, cand = 0, 0
 
     #     query_hashes.cache()
-    cand_set = set()
+    #     cand_set = set()
     length = len(query_hashes)
+    a = None
+    isFirst = True
 
     while True:
-        for t, v in data_hashes.groupByKey().collect():
+        #         for t, v in data_hashes.groupByKey().collect():
 
-            if t not in cand_set and countCol(v, query_hashes, offset, length) >= alpha_m:
-                cand_set.add(t)
+        #             if count(v, query_hashes, offset, length) >= alpha_m:
+        #                 cand_set.add(t)
+        if isFirst:
+            d = data_hashes.map(lambda x: c(x, query_hashes, offset, length, alpha_m))
+            a = d.filter(lambda x: x is not None)
+            isFirst = False
+            # print(a.collect())
+        else:
+            d = data_hashes.map(lambda x: c(x, query_hashes, offset, length, alpha_m))
+            d = d.filter(lambda x: x is not None)
+            a = a.union(d)
+            # print(type(a))
 
-        if len(cand_set) < beta_n:
+        if a.count() < beta_n:
             offset += 1
+            b = a.map(lambda x: (x, None))
+            data_hashes = data_hashes.subtractByKey(b)
         else:
             break
 
-    rdd = sc.parallelize(cand_set)
-    return rdd
+    #     rdd = sc.parallelize(cand_set)
+    return a
 
 
 start_time = time()
